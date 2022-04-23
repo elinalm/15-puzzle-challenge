16d24fefade5582453078788a74ff74e073b1a48
Elin <elin@zenio.se>
2022-04-23 10:22:19 +0200

    refactor: restructuring

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { PuzzleBoard } from './components/PuzzleBoard';
-
-test('renders learn react link', () => {
-  render(<PuzzleBoard />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
